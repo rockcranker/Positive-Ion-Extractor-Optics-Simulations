203fab1690ecac24bb7e7c557176bcf1e1227927
Morozov Igor <morozov@rat4.rfx.local>
2025-09-24 12:07:48 +0200

    Updated to run on RAT4, cleaned up to remove clutter

---
+++ BES_sim/BES_sim.cpp
@@ -0,0 +1,491 @@
+#include <random>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include "meshvectorfield.hpp"
+#include "dxf_solid.hpp"
+#include "mydxffile.hpp"
+#include "gtkplotter.hpp"
+#include "geomplotter.hpp"
+#include "geometry.hpp"
+#include "func_solid.hpp"
+#include "epot_efield.hpp"
+#include "error.hpp"
+#include "ibsimu.hpp"
+#include "trajectorydiagnostics.hpp"
+#include "particledatabase.hpp"
+#include "particlediagplotter.hpp"
+
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TLine.h"
+#include "TRandom.h"
+#include "TH1F.h"
+#include "TF1.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+#include <TGraph.h>
+#include <TVector.h>
+using namespace std;
+
+// Intersection of two lines, x and z directions only, y = 0
+Vec3D intPointZX(Vec3D p1, double m1, Vec3D p2, double m2){
+  double z1 = p1[2];
+  double x1 = p1[0];
+
+  double z2 = p2[2];
+  double x2 = p2[0];
+
+  double z = (m1 * z1 - m2 * z2 + x2 - x1)/(m1-m2);
+  double x = m1 * (z - z1) + x1;
+
+  return Vec3D(x, 0 , z);
+}
+
+// Angle between two vectors
+double vec2ang(Vec3D v1, Vec3D v2){
+  return acos((v1 * v2) /(v1.norm2() * v2.norm2()));
+}
+
+// Random uniform variable in bounds
+double getRandom(double min, double max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min, max);
+    return dis(gen);
+}
+
+// Random gaussian variable
+double getRandomGauss(double mean, double sigma){
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::normal_distribution<> dis(mean, sigma);
+  return dis(gen);
+}
+
+// Main simulation code
+void simu( int argc, char **argv )
+{
+  // Read input particle, geometry data
+  std::ifstream is_geom( "geom.dat" );
+  Geometry geom( is_geom );
+  is_geom.close();
+  
+  std::ifstream is_pdb( "pdb.dat" );
+  ParticleDataBase3D pdb( is_pdb, geom);
+  is_pdb.close();
+  
+  size_t npart = 0;
+  size_t pmax = pdb.size();
+  
+  vector<Particle3D> pvec;
+  vector<double> px;
+  vector<double> py;
+  vector<double> pz;
+  vector<double> pvx;
+  vector<double> pvy;
+  vector<double> pvz;
+  vector<double> pm;
+  vector<double> pI;
+
+  std::ifstream inputSet;
+  inputSet.open("parameters.txt");
+  std::string dirName;
+  std::string line;
+  double lSim;
+  if ( inputSet.is_open() ) {
+    inputSet >> dirName;
+    getline(inputSet, line);
+    getline(inputSet, line);
+    lSim = stod(line);
+  }
+  inputSet.close();
+
+  // Extract all particles exiting the beam simulation
+  for( size_t k = 0; k < pmax; k++ ) {
+
+    Particle3D &pp = pdb.particle( k );
+
+    // Skip ions not at the end
+    if( pp(PARTICLE_Z) < lSim - 0.0005 ){
+      continue;
+    }
+    
+    pvec.push_back(pp); // macroparticle object
+    px.push_back(pp(1)); // x
+    pvx.push_back(pp(2)); // v_x
+    py.push_back(pp(3)); // y 
+    pvy.push_back(pp(4)); // v_y
+    pz.push_back(pp(5)); // z
+    pvz.push_back(pp(6)); //v_z
+    pm.push_back(pp.m());  // mass
+    pI.push_back(pp.IQ()); // current
+    npart++;
+  }
+
+  vector<double> pE;
+  vector<double> xAng;
+  vector<double> yAng;
+
+  // Interaction rate parameters
+  double xs = 5e-22;
+  double uptime = 1; //s
+  double pressure = 0.03; //pa
+  double kb = 1.38e-23;
+  double numdens = pressure/(kb * 293.15);
+  double XS = xs * numdens;
+  
+  // Telescope position setup
+  double acceptance = 1.7e-3;
+  double optic_rad = 5e-3;
+  double beam_lower_bound = -0.02;
+  double xT = 0.099;
+  double xTl = xT - beam_lower_bound;
+  double theta = 45 * M_PI/180;
+  double del = 0.01;
+  double zTl = (xTl)/tan(theta);
+  double zT = lSim + del + zTl;
+  Vec3D vecT = Vec3D(xT,0,zT);
+  double Lmax = sqrt(xTl * xTl + zTl * zTl);
+
+  Vec3D vLOS = Vec3D(xTl, 0, zTl);
+  vLOS.normalize();
+  Vec3D off1 = Vec3D(0,0,1) - (Vec3D(0,0,1)*vLOS)*vLOS;
+  Vec3D off2 = cross(vLOS, off1);
+  double umin = cos(acceptance/2);
+  double u;
+  double moff;
+  double aoff;
+  Vec3D off;
+  Vec3D em;
+  Vec3D plane_int;
+  Vec3D plane_vec;
+  double d_int;
+
+  // simulation bundles parameters
+  int n_seg = 40;
+  double n_bund = 4;
+
+  // Stop particles
+  double BES_width = acceptance * Lmax + 2 * optic_rad;
+
+  // Starting plane
+  double xp1 = xT + optic_rad * cos(theta);
+  double zp1 = zT - optic_rad * sin(theta);
+  double yp1 = 0;
+  Vec3D Vp1 = Vec3D(xp1,yp1,zp1);
+  double mp1 = tan(theta - acceptance/2);
+
+  // Ending plane
+  double xp2 = xT - optic_rad * cos(theta);
+  double zp2 = zT + optic_rad * sin(theta);
+  double yp2 = 0;
+  Vec3D Vp2 = Vec3D(xp2,yp2,zp2);
+  double mp2 = tan(theta + acceptance/2);
+
+  double mx;
+  double my;
+  Vec3D pstart;
+  Vec3D pvel;
+  Vec3D int1;
+  Vec3D int2;
+  Vec3D P1;
+  Vec3D P2;
+  Vec3D track;
+  double Ltrack;
+  double Lstep;
+  double colls;
+  double bundle;
+
+  double vmag;
+
+  // Wavelength parameters
+  double alpha;
+  double beta;
+  double ha = 656.297; //nm
+  double qe = 1.602e-19;
+  double h;
+  double fpart;
+
+  // Output detections to text file
+  bool text_out = true;
+  int events_per_out = 1000;
+  ofstream fileBES( "BES_data.txt" );
+  if(text_out){
+    fileBES << setw(12) << "w";
+    fileBES << setw(12) << "alpha";
+    fileBES << setw(12) << "beta";
+    fileBES << setw(12) << "h";
+    fileBES << setw(12) << "m ";
+    fileBES << "\n";
+  }
+
+  vector<Vec3D> P1el;
+  vector<Vec3D> P2el;
+  vector<Vec3D> draw_sample;
+
+  vector<double> H;
+  vector<double> W;
+
+  Vec3D travel;
+  double track_fraction;
+  Vec3D seg;
+  Vec3D seg2tel;
+
+  unsigned long long detections = 0;
+  int particles_stopped = 0;
+  int draw_count = 0;
+
+  // Find particle trajectories, simulate interactions
+  for (size_t j = 0; j < npart; j++){
+    if(j % 100000 == 0){
+      cout << "particle " << j << "\n";
+      cout << detections << "\n";	
+    }
+
+    // find intersection with planes to define simulated track
+    pstart = Vec3D(px[j], py[j], pz[j]);
+    pvel = Vec3D(pvx[j], pvy[j], pvz[j]);
+    vmag = pvel.norm2();
+    my = pvel[1]/pvel[2];
+    mx = pvel[0]/pvel[2];
+
+    int1 = intPointZX(pstart, mx, Vp1, mp1);
+    P1 = Vec3D(int1[0], pstart[1] + my * (int1[2] - pstart[2]), int1[2]);
+    int2 = intPointZX(pstart, mx, Vp2, mp2);
+    P2 = Vec3D(int2[0], pstart[1] + my * (int2[2] - pstart[2]), int2[2]);
+    track = P2 - P1;
+    Ltrack = track.norm2();
+
+    // Discard particles based on y and z
+    if (abs(P1[1]) >= BES_width){
+      continue;
+    }
+    if (P1[2] <= lSim){
+      cout << "Discarded particle based on z < lsim \n";
+      continue;
+    }
+    
+    P1el.push_back(P1);
+    P2el.push_back(P2);
+    particles_stopped++;
+
+    // divide track into segments, emissions from segment into bundle
+    fpart = pI[j] / qe;
+    Lstep = Ltrack/n_seg;
+    colls = Lstep*XS*fpart* uptime;
+    colls = colls * (acceptance * acceptance)/16;
+    bundle = colls/n_bund;
+
+    // simulate emissions
+    for(int k = 0; k < n_seg; k++){
+      track_fraction = (k + 0.5)/n_seg;
+      travel = track_fraction * track;
+      seg = P1 + travel;
+      seg2tel = vecT - seg;
+
+      for(int l = 0; l < n_bund; l++){
+	// sample direction
+	u = getRandom(umin, 1);
+	moff = tan(acos(u));
+	aoff = getRandom(0, 2*M_PI);
+	off = sin(aoff) * moff * off1 + cos(aoff) * moff * off2;
+	em = vLOS + off;
+	em.normalize();
+
+	// find intersection
+	plane_int = seg + (((seg2tel)*vLOS)/(em*vLOS))*em;
+	plane_vec = plane_int - vecT;
+	d_int = plane_vec.norm2();
+
+	// record detection if in range
+	if (d_int <= optic_rad){
+	  // calculate wavelength
+	  alpha = vec2ang(pvel, em);
+	  beta = vmag/(299792458);
+	  // ripple effect
+	  beta = beta * (1 + getRandomGauss(0, 0.01)); // 1% ripple
+	  h = ha * (1 - beta*cos(alpha))/(sqrt(1 - beta * beta));
+	  // random offset
+	  h += getRandomGauss(0, 0.025); // 25 picometers error
+	  
+	  W.push_back(bundle);
+	  H.push_back(h);
+
+	  // visualization
+	  if(detections % 999 == 0){
+	    draw_sample.push_back(seg);
+	    draw_count++;
+	  }
+
+	  // text output
+	  if(text_out && (detections % events_per_out == 0)){
+	      fileBES << setw(12) << bundle;
+	      fileBES << setw(12) << alpha;
+	      fileBES << setw(12) << beta;
+	      fileBES << setw(12) << h;
+	      fileBES << setw(12) << pm[j];
+	      fileBES << "\n";
+	   }
+
+	  detections++;
+	}
+      }
+    }
+    
+  }
+  
+  // Construct unshifted peak
+  // Physical constants
+  constexpr double k_B = 1.380649e-23;      // Boltzmann constant (J/K)
+  constexpr double T_b = 298.0;               // Temperature in K
+  constexpr double m_2 = 3.35e-27;            // Mass of H2 molecule in kg
+  double sigma = sqrt(k_B * T_b / m_2);
+
+  double unshift_frac = 0.35;
+  int n_unshift = (int)detections * unshift_frac;
+  
+  double vx;
+  double vy;
+  double vz;
+  
+  vector<double> HU;
+  vector<double> WU;
+  
+  for(int m = 0; m < n_unshift; m++){
+	  vx = getRandomGauss(0, sigma);
+	  vy = getRandomGauss(0, sigma);
+	  vz = getRandomGauss(0, sigma);
+	  vmag = sqrt( vx * vx + vy * vy  + vz * vz);
+	  
+	  alpha = atan((sqrt(vx * vx + vy * vy))/vz);
+	  beta = vmag/(299792458);
+	  h = ha * (1 - beta*cos(alpha))/(sqrt(1 - beta * beta));
+	  h += getRandomGauss(0, 0.025); // 25 picometers error
+	  
+	  WU.push_back(W[m]);
+	  HU.push_back(h);
+  }
+  
+  fileBES.close();
+  cout << "Total particles stopped: " << particles_stopped << "\n";
+  cout << "Total events detected: " << detections << "\n";
+  cout << "Drawing particles" << draw_count << "\n";
+  
+  
+  // Add to parameter file
+  ofstream param("parameters.txt", std::ios::app);
+  param << "BES angle: " << theta << "\n";
+  param << "Acceptance angle: " << acceptance << "\n";
+  param << "Optical diameter: " << 2*optic_rad << "\n";
+  param.close();
+  
+  // Histogram setup
+  cout << "Creating hist \n";
+  double max_h = *std::max_element(HU.begin(), HU.end());
+  double min_h = *std::min_element(H.begin(), H.end());
+  size_t nbins = 1000; //  should be set as instrumental bin width
+
+  cout << "Creating file \n";
+  TFile fout("results.root","recreate");
+
+  auto h1 = new TH1D("h1", "BES Results", nbins, min_h,max_h);
+  h1->FillN(detections,H.data(),W.data());
+  h1->FillN(n_unshift, HU.data(), WU.data());
+
+  int b_max = h1->GetMaximumBin();
+  TLine *line_ha = new TLine(ha, 0, ha, h1->GetBinContent(b_max));
+  line_ha->SetLineColor(kRed);
+  line_ha->SetLineWidth(4);
+  h1->SetLineWidth(4);
+
+  TCanvas *ch = new TCanvas("ch", "Canvas BES Hist", 10000, 5000);
+  h1->SetStats(0);
+  h1->Draw("hist");
+  line_ha->Draw();
+  cout << "Creating graph \n";
+  ch->SaveAs("BES_out.png");
+  fout.WriteObject(h1, "h1");
+  fout.Close();
+
+
+  // Write histogram data to file
+  cout << "Hist data file \n";
+  ofstream histdata("histdata.txt");
+  double low_edge, high_edge, bin_counts;
+  for(size_t bin = 1; bin < nbins; bin++){
+    low_edge = h1->GetBinLowEdge(bin);
+    high_edge = h1->GetBinLowEdge(bin+1);
+    bin_counts = h1->GetBinContent(bin);
+    histdata << low_edge << "\t" << high_edge << "\t" << bin_counts << "\n";
+  }
+  histdata.close();
+  
+  
+  // visualize stopped particle trajectories, detections, in z-x and z-y
+  bool BES_sample_viz = false;
+  if (BES_sample_viz){
+	  cout << "visualizing sample \n";
+    int nBES = P1el.size();
+    double xarr[2*nBES];
+    double yarr[2*nBES];
+    double zarr[2*nBES];
+    double collx[draw_count];
+    double colly[draw_count];
+    double collz[draw_count];
+    for(int i = 0; i < nBES; i++){
+      xarr[i] = P1el[i][0];
+      yarr[i] = P1el[i][1];
+      zarr[i] = P1el[i][2];
+    }
+    for(int i = 0; i < nBES; i++){
+      xarr[nBES+i] = P2el[i][0];
+      yarr[nBES+i] = P2el[i][1];
+      zarr[nBES+i] = P2el[i][2];
+    }
+    for(int i = 0; i < draw_count; i++){
+      collx[i] = draw_sample[i][0];
+      colly[i] = draw_sample[i][1];
+      collz[i] = draw_sample[i][2];
+    }
+    TGraph *g = new TGraph(2*nBES, zarr, xarr);
+    TCanvas *c1 = new TCanvas("c1", "z-x", 800, 400);
+    c1->SetFixedAspectRatio();
+    g->Draw("AP");
+    c1->SaveAs("z_x.png");
+    TGraph *g2 = new TGraph(nBES, zarr, yarr);
+    TCanvas *c2 = new TCanvas("c2", "z-y", 800, 400);
+    c2->SetFixedAspectRatio();
+    g2->Draw("AP");
+    c2->SaveAs("z_y.png");
+    TGraph *g3 = new TGraph(draw_count, collz, collx);
+    TCanvas *c3 = new TCanvas("c3", "detections zx", 1920, 1080);
+    c3->SetFixedAspectRatio();
+    g3->Draw("AP");
+    c3->SaveAs("detections zx.png");
+    TGraph *g4 = new TGraph(draw_count, collz, colly);
+    TCanvas *c4 = new TCanvas("c4", "detections zy", 1920, 1080);
+    c4->SetFixedAspectRatio();
+    g4->Draw("AP");
+    c4->SaveAs("detections zy.png");
+  }
+
+  
+}
+
+
+int main( int argc, char **argv )
+{
+  ibsimu.set_message_threshold( MSG_VERBOSE, 1 );
+  ibsimu.set_thread_count( 4 );
+  simu( argc, argv );
+  return( 0 );
+}
---
+++ Triode3D/triode3d.cpp
@@ -0,0 +1,326 @@
+#include "epot_bicgstabsolver.hpp"
+#include "particledatabase.hpp"
+#include "geometry.hpp"
+#include "func_solid.hpp"
+#include "epot_efield.hpp"
+#include "meshvectorfield.hpp"
+#include "ibsimu.hpp"
+#include "error.hpp"
+#include "particlediagplotter.hpp"
+#include "geomplotter.hpp"
+#include "config.h"
+#include "gtkplotter.hpp"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+// Plasma parameters
+const double Te = 5.; // Electron temperature
+const double Ti = 0.1; // Ion temperature, can be eparated into tranvere and longitudinal if needed
+const double Up = 5.0;  // Plasma potential
+
+// Extractor geometry, in meters
+const double a = 0.009; // Spacing PG to EG
+const double b = 0.002; // Spacing EG to GG
+const double t1 = 0.002; // PG thickness
+const double t2 = 0.003; // EG and GG thickness
+
+const double wSlit = 0.036; // Slit width
+const double hSlit = 0.004; // Slit height
+
+// Extractor configuration
+const double Vext = -10e3; // Total extraction voltage
+const double dVgg = 300.0; // Diffference between EG and GG voltage
+const double J = 330.0; // Extracted current
+
+// B field components, uniform, Tesla
+double Bx = 0.0;
+double By = 0.0;
+double Bz = 0.0;
+
+// Space charge neutralization past extractor
+const bool SC_neutralization = true;
+
+// SImulation domain setup
+const double lAcc = 0.001 + t1 + a + t2 + b + t2 + 0.001;
+const double lSim = lAcc; // simulation length, z
+const double wSim = 0.048; // Simulation width, x
+const double hSim = 0.014; // Simulation height, y
+
+const double lMesh = 0.0004; // Cubic mesh size, m
+const int ppc = 45; // Particles per mesh cell in beam
+const double H1_frac = (double)1/3; // Fraction of H1+
+const double H2_frac = (double)1/3; // Fraction of H1+
+const double H3_frac = (double)1/3; // Fraction of H1+
+
+// Beam size, can be smaller than Sim size, but must be larger than slit
+const double wBeam = 0.048;
+const double hBeam = 0.014;
+
+// Cell size and beam calculations
+const int nCellW = (int)(wSim/lMesh);
+const int nCellH = (int)(hSim/lMesh);
+const int nCellL = (int)(lSim/lMesh);
+const int NBeam = (int)(wBeam/lMesh) * (int)(hBeam/lMesh) * ppc; // Expected particles in beam
+const int N1 = (int)NBeam*H1_frac;
+const int N2 = (int)NBeam*H2_frac;
+const int N3 = (int)NBeam*H3_frac;
+const int N = N1 + N2 + N3; // Actual particle number
+const int NSlit = (int)(wSlit/lMesh) * (int)(hSlit/lMesh) * ppc; // Expected particles exracted from slit
+
+
+// Simulation iteration cycles
+const int N_cycles = 20;
+
+// PG geometry w/ Pierce angle 
+bool solid1( double x, double y, double z )
+{
+  return ( (z <= 0.001 + t1 && z >= 0.001) &&
+	   ((x <= -0.02 || x >= 0.02 || y <= -0.00296 || y >= 0.00296) ||
+	   ((z >= 0.001 && z <= 0.0024) && !(std::abs(x) <= 0.018 || std::abs(x) <= 5*z + 0.008)) ||
+	    ((z >= 0.001 && z <= 0.0024) && !(std::abs(y) <= 0.002 ||  abs(y) <= 2.4*z - 0.0028))
+	    ));
+}
+
+// EG geometry with rounded slit
+bool solid2( double x, double y, double z )
+{
+  double w1 = 0.04;
+  double h1 = 0.003;
+  double r1 = (x - (w1-h1)/2)*(x - (w1-h1)/2) + y * y;
+  double r2 = (x + (w1-h1)/2)*(x + (w1-h1)/2) + y * y;
+
+  return  ( z <= a + t1 + t2 + 0.001 && z >= a + t1 + 0.001 &&
+	    (y >= h1/2 || y <= -h1/2 || x >= (w1-h1)/2 || x <= -(w1-h1)/2) &&
+	    r1 >= (h1/2)*(h1/2) &&   r2 >= (h1/2)*(h1/2)
+	    );
+}
+
+// GG geometry with rounded slit
+bool solid3( double x, double y, double z )
+{
+  double w1 = 0.04;
+  double h1 = 0.0036;
+  double r1 = (x - (w1-h1)/2)*(x - (w1-h1)/2) + y * y;
+  double r2 = (x + (w1-h1)/2)*(x + (w1-h1)/2) + y * y;
+  return  ( z <= a + t1 + t2 + t2 + b + 0.001 && z >= a + 0.001 + t1 + t2 + b &&
+	    (y >= h1/2 || y <= -h1/2 || x >= (w1-h1)/2 || x <= -(w1-h1)/2) &&
+	    r1 >= (h1/2)*(h1/2) &&   r2 >= (h1/2)*(h1/2)
+	    );
+}
+
+void simu( int *argc, char ***argv )
+{
+  // Geometry set-up
+  Geometry geom( MODE_3D, Int3D(nCellW + 1, nCellH + 1, nCellL + 1), Vec3D(-wSim/2,-hSim/2,0), lMesh); // Simulation domain
+  // Grids
+  Solid *s1 = new FuncSolid( solid1 );
+  geom.set_solid( 7, s1 );
+  Solid *s2 = new FuncSolid( solid2 );
+  geom.set_solid( 8, s2 );
+  Solid *s3 = new FuncSolid( solid3 );
+  geom.set_solid( 9, s3 );
+  // BC
+  geom.set_boundary( 1, Bound(BOUND_NEUMANN,    0.0) ); // negative x
+  geom.set_boundary( 2, Bound(BOUND_NEUMANN,    0.0) ); // positive x
+  geom.set_boundary( 3, Bound(BOUND_NEUMANN,    0.0) ); // negative y
+  geom.set_boundary( 4, Bound(BOUND_NEUMANN,    0.0) ); // positive y
+  geom.set_boundary( 5, Bound(BOUND_NEUMANN,    0.0) ); // negative z
+  geom.set_boundary( 6, Bound(BOUND_DIRICHLET, Vext) ); // positive z
+  geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  Up)  ); // PG
+  geom.set_boundary( 8, Bound(BOUND_DIRICHLET, Vext - dVgg) ); // EG
+  geom.set_boundary( 9, Bound(BOUND_DIRICHLET, Vext) ); // GG
+  geom.build_mesh();
+    
+  EpotBiCGSTABSolver solver( geom );
+  
+  // Inital plasma region
+  InitialPlasma initp( AXIS_Z, 3e-3 );
+  solver.set_initial_plasma( Up, &initp );
+
+  // E field
+  EpotField epot( geom );
+  MeshScalarField scharge( geom );
+  EpotEfield efield( epot );
+  field_extrpl_e efldextrpl[6] = { FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE, 
+            FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE,
+            FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
+  efield.set_extrapolation( efldextrpl );
+
+  // B field, uniform
+  bool fout[3] = { true, true, true };
+  MeshVectorField bfield( geom, fout );
+  for( uint32_t i = 0; i < bfield.size(0); i++ ) {
+    for( uint32_t j = 0; j < bfield.size(1); j++ ) {
+      for ( uint32_t k = 0; k < bfield.size(2); k++ ) {
+        bfield.set( i, j, k, Vec3D( Bx, By, Bz ) );
+      }
+    }
+  }
+
+  // Particle setup
+  ParticleDataBase3D pdb( geom );
+  bool pmirror[6] = { false, false, false, false, false, false };
+  pdb.set_mirror( pmirror );
+
+  // Iterative solver
+  for( size_t i = 0; i < N_cycles; i++ ) {
+    if( i == 1 ) {
+        double rhoe = pdb.get_rhosum();
+        solver.set_pexp_plasma( -rhoe, Te, Up );
+    }
+
+
+    // Space Charge Suppression past accelerator
+    if (SC_neutralization){
+      for( uint32_t k = 0; k < scharge.size(0); k++ ) {
+        for( uint32_t l = 0; l < scharge.size(1); l++ ) {
+          for ( uint32_t m = 0; m < scharge.size(2); m++ ) {
+            if ((m+1) * lMesh > lAcc){
+              scharge( k, l, m) = 0;
+            }
+          }
+        }
+      }
+    }
+
+    solver.solve( epot, scharge );
+    efield.recalculate();
+
+    // Particle beam
+    pdb.clear();
+    // H1+
+    pdb.add_rectangular_beam_with_energy( N1, J * H1_frac, 1.0, 1.0, 
+                  5.0, Ti, Ti, 
+                  Vec3D(0,0,0), Vec3D(1,0,0), Vec3D(0,1,0), 
+                  wBeam/2, hBeam/2);
+    // H2+
+    pdb.add_rectangular_beam_with_energy( N2, J * H2_frac, 1.0, 2.0, 
+                  5.0, Ti, Ti, // Initial energy and temperature
+                  Vec3D(0,0,0), Vec3D(1,0,0), Vec3D(0,1,0), 
+                  wBeam/2, hBeam/2);
+    // H3+
+    pdb.add_rectangular_beam_with_energy( N3, J * H3_frac, 1.0, 3.0, 
+                  5.0, Ti, Ti, 
+                  Vec3D(0,0,0), Vec3D(1,0,0), Vec3D(0,1,0), 
+                  wBeam/2, hBeam/2 );
+    pdb.iterate_trajectories( scharge, efield, bfield );
+
+    // Optional plotting of intermediate results
+    /*
+    if (i == 8){
+      GeomPlotter geomplotter1( geom );
+      geomplotter1.set_size( 1280, 1280 );
+      geomplotter1.set_epot( &epot );
+      geomplotter1.set_particle_database( &pdb );
+      geomplotter1.set_view( VIEW_ZY);
+      geomplotter1.plot_png( "plot_zy_intermediate.png" );
+    }
+    */
+  }
+  
+  // Output files
+  geom.save( "geom.dat" );
+  epot.save( "epot.dat" );
+  pdb.save( "pdb.dat" );
+
+  // Write output file containing parameters
+  
+  std::ostringstream oss; // Create an output string stream object
+  
+  std::ofstream fileOut( "parameters.txt" );
+  
+  // convert spacing to string
+  std::string astr;
+  oss << (a*1000);    
+  astr =  oss.str();
+  astr.erase(std::remove(astr.begin(), astr.end(), '.'), astr.end());
+  astr.erase(astr.find_last_not_of("0") + 1, std::string::npos);
+  oss.str("");   // Clear the content
+  oss.clear();   // Clear the state flags
+  
+  // convert temperature to string
+  std::string Tstr;
+  oss<<(Ti);
+  Tstr = oss.str();
+  std::replace(Tstr.begin(), Tstr.end(), '.','-');
+  Tstr.erase(Tstr.find_last_not_of("0") + 1, std::string::npos);
+
+  // Write parameters to file
+  fileOut << "J" << J << "a" << astr << "N" << NSlit << "T" << Tstr << "\n"; // Name string with relevant parameters
+  fileOut << lSim << "\n"; // Simulation length, read by postp and BES_sim programs
+  // any relevant parameters output for future reference
+  fileOut << a << "\n";
+  fileOut << "a: " << a << ",   ";
+  fileOut << "b: " << b << ",   ";
+  fileOut << "t1: " << t1 << ",   ";
+  fileOut << "t2: " << t2 << ",   ";
+  fileOut << "\n";
+  fileOut << "Voltage: " << Vext << " ";
+  fileOut << "\n";
+  fileOut << "Current: " << J << " ";
+  fileOut << "\n";
+  fileOut << "N simulated: " << NBeam << "\n";
+  fileOut << "N extracted estimate: " << NSlit << "\n";
+  fileOut << "Mesh size: " << lMesh << "\n";
+  fileOut << "Particles per cell: " << ppc << "\n";
+  fileOut << "Particle ratios: \nH1: " << H1_frac << "\t\tH2: " << H2_frac << "\t\tH3: " << H2_frac << "\n";
+  fileOut.close();
+  
+  // Trajectory plots
+  GeomPlotter geomplotter( geom );
+  geomplotter.set_size( 1280, 1280 );
+  geomplotter.set_epot( &epot );
+  geomplotter.set_particle_database( &pdb );
+  geomplotter.set_view( VIEW_ZX);
+  geomplotter.plot_png( "plot_zx.png" );
+  geomplotter.set_view( VIEW_ZY);
+  geomplotter.plot_png( "plot_zy.png" );
+
+  // Particle profile cross section in center of grids
+  //geomplotter.set_view( VIEW_XY, (int)(0.0135/lMesh));
+  //geomplotter.plot_png( "grid1xs.png" );
+  //geomplotter.set_view( VIEW_XY, (int)(0.0185/lMesh));
+  //geomplotter.plot_png( "grid2xs.png" );
+  
+  // Interactive plotter
+  if( false ) { // change to true to enable
+      MeshScalarField tdens( geom );
+      pdb.build_trajectory_density_field( tdens );
+      GTKPlotter plotter( argc, argv );
+      plotter.set_geometry( &geom );
+      plotter.set_epot( &epot );
+      plotter.set_bfield( &bfield );
+      plotter.set_efield( &efield );
+      plotter.set_scharge( &scharge );
+      plotter.set_trajdens( &tdens );
+      plotter.set_particledatabase( &pdb );
+      plotter.new_geometry_plot_window();
+      plotter.new_particle_plot_window( AXIS_Z, 0.03, PARTICLE_DIAG_PLOT_SCATTER, DIAG_VZ, DIAG_VX);
+      plotter.run();	
+  }
+  
+}
+
+// main simulation code
+int main( int argc, char **argv )
+{
+  try {
+      ibsimu.set_message_threshold( MSG_VERBOSE, 1 );
+      ibsimu.set_thread_count( 4 );
+      simu( &argc, &argv );
+  } catch ( Error e ) {
+      e.print_error_message( ibsimu.message( 0 ) );
+      exit( 1 );
+  }
+
+  return( 0 );
+}
